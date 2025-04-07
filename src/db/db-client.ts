29c62e49ea3cfac7674503e58776203bf423bb46
Andrew Tolentino <atolentino@hawk.iit.edu>
2025-04-08 07:45:36 +0900

    Nit

--- src/db/db-client.ts
+++ src/db/db-client.ts
@@ -1,8 +1,6 @@
 import { YouOweEntity } from '@/entities/entity'
 
-/**
- * Base Interface that will be used throughout the application to hide implementation details per DB client provider
- */
+/** Base Interface that will be used throughout the application to hide implementation details per DB client provider. */
 export interface DBClient {
   getEntityById(tableName: string, id: string): Promise<YouOweEntity | null>
   createEntityById(tableName: string, entity: YouOweEntity): Promise<YouOweEntity | null>
