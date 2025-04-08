90cc4ada79175a9d798cd02802f49131169cefba
Andrew Tolentino <atolentino@hawk.iit.edu>
2025-04-08 09:18:40 +0900

    create members table

---
+++ supabase/migrations/20250406234921_table_create_members.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "public"."members" (
+  "id" varchar(24) NOT NULL DEFAULT generate_bson_id() PRIMARY KEY,
+  "name" varchar(50) NOT NULL,
+  "created_at" timestamp WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+  "deleted_at" timestamp WITH TIME ZONE,
+  "auth_user_id" uuid NOT NULL UNIQUE,
+
+  CONSTRAINT fk_user_profiles_auth_users_id
+    FOREIGN KEY("auth_user_id")
+      REFERENCES "auth"."users"(id)
+      ON DELETE CASCADE
+);
