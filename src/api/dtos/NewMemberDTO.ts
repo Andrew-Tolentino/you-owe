3e81bd0073eb224826c4cf96e448192f6e44a48f
Andrew Tolentino <atolentino@hawk.iit.edu>
2025-04-10 07:12:14 +0900

    Nit

--- src/api/dtos/NewMemberDTO.ts
+++ src/api/dtos/NewMemberDTO.ts
@@ -13,8 +13,9 @@ export interface NewMemberDTO {
  * @returns {string | null} Returns an error message string if there was an issue found in DTO. Else, returns null.
  */
 export function validateNewMemberDTO(DTO: NewMemberDTO): string | null {
-  if (!isString(DTO.name)) {
-    return "'name' field is invalid"
+  const { name } = DTO
+  if (!isString(name)) {
+    return "'name' field is invalid."
   }
 
   return null
