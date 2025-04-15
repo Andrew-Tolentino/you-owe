afb1f8a4d1d302b64fc841f211c4dce3df547943
Andrew Tolentino <atolentino@hawk.iit.edu>
2025-04-16 08:07:24 +0900

    Nit comment

--- supabase/db-functions/set-updated-at.sql
+++ supabase/db-functions/set-updated-at.sql
@@ -1,4 +1,4 @@
--- set_updated_at() is a general trigger use set the 'updated_at' column for tables
+-- General trigger use set the 'updated_at' column for tables
 CREATE OR REPLACE FUNCTION "public".set_updated_at() RETURNS TRIGGER AS $$
   BEGIN
     NEW.updated_at = timezone('utc', now());
