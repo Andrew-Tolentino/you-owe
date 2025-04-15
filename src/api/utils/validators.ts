9e2a6fc60c333d764d5358147b91dc0e8f49749e
Andrew Tolentino <atolentino@hawk.iit.edu>
2025-04-15 12:30:55 +0900

    Refactor to first check for strings

--- src/api/utils/validators.ts
+++ src/api/utils/validators.ts
@@ -1,16 +1,18 @@
 /**
  * Checks if the given argument is of type string.
+ * Will apply trimming to the argument as part of the checking.
  * 
  * @param {string} str
  * 
- * @returns {boolean} - Returns true if string, else false. Also returns false if empty string.
+ * @returns {boolean} Returns true if string, else false. Also returns false if empty string.
  */
 export function isString(str: string): boolean {
-  if (str === '') {
+  if (typeof str !== 'string') {
     return false
   }
 
-  if (typeof str !== 'string') {
+  // Check if an empty string
+  if (str.trim() === '') {
     return false
   }
 
